da3ffe48a6b8b1151cbdab4cdb7fc3b3d77b205c
RaphaAmericano <raphael@raphaelamericano.com.br>
2017-05-13 01:52:52 -0300

    Incluindo a interface Utilitarios e outros consertos

---
+++ src/arvore_genealogica/Utilitarios.java
@@ -0,0 +1,23 @@
+package arvore_genealogica;
+//Importar o pacote de funções matérmaticas
+
+
+public interface Utilitarios {
+	public void fazerFilho(Object affair){
+		int numeroRandomico;//atribuir um numero randomico de 0 a 10 a essa variavel usando uma variavel usando a função do pacote importado
+		//int numeroPessoas = TestePessoa.todasPessoas; //Numero de pessoas
+		if(numeroRandomico >= 5 ){
+			Pessoa m1 = new Mulher();
+		} else if(numeroRandomico < 5){
+			Pessoa m1 = new Homem();
+		};
+		if(this.getSexo == 'M'){
+			m1.setPai(this);
+			m1.setMae(affair);
+		} else if (this.getSexo == 'F'){
+			m1.setMae(this);
+			m1.setPai(affair);
+		}
+		
+	}
+}
