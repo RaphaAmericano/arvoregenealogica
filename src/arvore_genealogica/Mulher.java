d46d6bb1396b1c18af96f7ca409c38dd24baf069
Andre Pfeiffer <andre@pfeiffer.com.br>
2017-05-21 13:39:01 -0300

    formatação da saida do IMC

--- src/arvore_genealogica/Mulher.java
+++ src/arvore_genealogica/Mulher.java
@@ -1,5 +1,7 @@
 package arvore_genealogica;
 
+import java.text.DecimalFormat;
+
 public class Mulher extends Pessoa implements ControledePeso {
 	
 	// Caracteristicas exclusivas de Mulher
@@ -19,27 +21,28 @@ public class Mulher extends Pessoa implements ControledePeso {
 	// Implementa o método Calcular IMC
 	public void calcularIMC(){
 		double imc =  this.getPeso() / (this.getAltura() * this.getAltura());
+		DecimalFormat df = new DecimalFormat("#.##");
 		
 		if(imc >= 40.00){
-			System.out.println(this.nome + ": IMC = " + imc + " - Obesidade 3(Mórbida)");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Obesidade 3(Mórbida)");
 			return;
 		}else if(imc >= 35.00){
-			System.out.println(this.nome + ": IMC = " + imc + " - Obesidade 2(Severa)");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Obesidade 2(Severa)");
 			return;
 		}else if(imc >= 30.00){
-			System.out.println(this.nome + ": IMC = " + imc + " - Obesidade 1");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Obesidade 1");
 			return;
 		}else if(imc >= 25.00){
-			System.out.println(this.nome + ": IMC = " + imc + " - Acima do peso");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Acima do peso");
 			return;
 		}else if(imc >= 18.50){
-			System.out.println(this.nome + ": IMC = " + imc + " - Peso normal");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Peso normal");
 			return;
 		}else if(imc >= 17){
-			System.out.println(this.nome + ": IMC = " + imc + " - Abaixo do peso");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Abaixo do peso");
 			return;
 		}else {
-			System.out.println(this.nome + ": IMC = " + imc + " - Muito abaixo do peso");
+			System.out.println(this.nome + ": IMC = " + df.format(imc) + " - Muito abaixo do peso");
 			return;
 		}
 		//Informações acima sobre tipos de obesidade foram retiradas da internet e não necessariamente foram verificadas
