b178b05f7f4744b1e05af69913bbe63e99ca460b
Andre Pfeiffer <andre@pfeiffer.com.br>
2017-05-21 02:44:21 -0300

    arrumando testes 2

--- src/arvore_genealogica/TestePessoa.java
+++ src/arvore_genealogica/TestePessoa.java
@@ -7,61 +7,6 @@ import java.util.List;
 public class TestePessoa {
 	public static List<Pessoa> todasPessoas = new ArrayList<Pessoa>();
 	public static void main(String[] args) {
-		/*
-		Pessoa m1 = new Pessoa("Dona Florinda", 60,  null, null );
-		Pessoa h1 = new Pessoa("Professor Girafales", 65, null, null);
-		Pessoa h2 = new Pessoa("Quico", 10, m1, null);
-		Pessoa h3 = new Pessoa("Seu Barriga", 68, null, null);
-		Pessoa h4 = new Pessoa("Nhonho", 12, null, h3);
-		Pessoa m2 = new Mulher();
-		//Testes
-		//Familia 1
-		Pessoa th1 = new Pessoa("Jorge", 60, null, null);
-		Pessoa tm1 = new Pessoa("Joana", 56, null, null);
-		th1.casar(tm1);
-		Pessoa th2 = new Pessoa("Paulo", 40, tm1, th1 );
-		Pessoa tm2 = new Pessoa("Maria", 38, tm1, th1);
-		Pessoa th3 = new Pessoa("Mario", 35, tm1, th1);
-		//Familia 2
-		Pessoa th4 = new Pessoa("Carlos", 62, null, null);
-		Pessoa tm3 = new Pessoa("Joaquina", 58, null, null);
-		th4.casar(tm3);
-		Pessoa th5 = new Pessoa("Carlinhos", 45, tm3, th4);
-		Pessoa tm4 = new Pessoa("Luiza", 40, tm3, th4);
-		Pessoa tm5 = new Pessoa("Valesca", 38, tm3, th4);
-		
-		th2.casar(tm5);
-		th5.casar(tm2);
-		th3.casar(tm4);
-		
-		//Criancas
-		Pessoa th6 = new Pessoa("Carlos Neto", 12, tm2, th5);
-		Pessoa tm6 = new Pessoa("Fabiana", 8, tm2, th5);
-		Pessoa th7 = new Pessoa("João Petro", 14, tm4, th3);
-		//Teste IMC Mariazinha
-		Pessoa tm7 = new Mulher();
-		Pessoa th8 = new Homem();
-		
-		tm7.setNome("Mariazinha Linda");
-		tm7.setAltura(180.00);
-		tm7.setPeso(205.00);
-		
-		th7.verificarParentesco(th3);
-		m1.casar(h1);
-		m1.verificarEstadoCivil();
-		//h1.divorciar(m1);
-		
-		//th1.listarFilhos();
-		//tm7.calcularIMC();
-		//System.out.println(todasPessoas.size());
-		System.out.println(th8.getSexo());
-		th8.fazerFilho();
-		th8.listarFilhos();
-
-		for(int i = 0; i < todasPessoas.size(); i++){
-			System.out.println(todasPessoas.get(i).getNome());
-		}
-		*/
 		
 		// Árvore OFICIAL - Família Chaves
 
